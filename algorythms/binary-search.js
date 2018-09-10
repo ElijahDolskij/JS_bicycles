62d1652d320b202438e2de7895f3b8e451711500
Илья <ElijahDolskij@gmail.com>
2018-09-10 14:00:34 +0300

    Binary search implementation

--- algorythms/binary-search.js
+++ algorythms/binary-search.js
@@ -1,10 +1,10 @@
-const bynaryDataCreator = (amountOfItems = 10, numsLength = 10, cashedArr) => {
+const bynaryDataCreator = (amountOfItems = 10, numsLength = 10) => {
   if (amountOfItems > numsLength) {
     return '"amountOfItems" must be less than "numsLength"!' +
       'Please, try again.'
   }
 
-  let result = cashedArr ? cashedArr : []
+  let result = []
 
   while (result.length < amountOfItems) {
     let newVal = Math.round(Math.random() * numsLength)
@@ -13,12 +13,29 @@ const bynaryDataCreator = (amountOfItems = 10, numsLength = 10, cashedArr) => {
     }
   }
 
-  if (result.length !== amountOfItems) {
-    result = [
-      ...result,
-      ...bynaryDataCreator(amountOfItems - result.length, numsLength, result)
-    ]
+  return result.sort((a, b) => a - b)
+}
+
+let testArr = bynaryDataCreator(6, 10)
+
+const binarySearch = (arr, val) => {
+  if (arr.length === 0 || !val) return
+  let min = 0
+  let max = arr.length - 1
+  let mid = Math.floor(max / 2)
+  let index = false;
+
+  while (mid >= min && !index) {
+    if (arr[mid] === val) {
+      index = mid
+    } else if (arr[mid] > val) {
+      max = mid - 1
+      mid = Math.floor(min + (max / 2))
+    } else {
+      min = mid + 1
+      mid = Math.floor((min + max) / 2)
+    }
   }
 
-  return result.sort((a, b) => a - b)
+  return index || 'The search value is missing from the target array'
 }
